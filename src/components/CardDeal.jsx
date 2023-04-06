2283629f5b2b003764067dcd12902bd0a4052d15
GabrielRoguski <gabrielc.o.roguski@hotmail.com>
2023-04-06 19:35:55 -0300

    first commit

---
+++ src/components/CardDeal.jsx
@@ -0,0 +1,25 @@
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      Discover the perfect card tailored to your needs by comparing various options, benefits, and features. Our user-friendly platform simplifies the process, guiding you towards the ideal card that aligns with your financial goals and lifestyle preferences.
+      </p>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="billing" className="w-[100%] h-[100%]" />
+    </div>
+  </section>
+);
+
+export default CardDeal;
